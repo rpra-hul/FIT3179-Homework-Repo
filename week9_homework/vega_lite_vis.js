fe5d0d58945e1b89437478700df50973201b6afe
oghul <105495412+oghul@users.noreply.github.com>
2024-09-22 13:47:42 +0800

    Cleaned up files for submission.

--- week9_homework/vega_lite_vis.js
+++
@@ -1,5 +0,0 @@
-var vg_1 = "homework_week9.vg.json";
-
-vegaEmbed("#map_chart", vg_1).then(function(result) {
-  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
